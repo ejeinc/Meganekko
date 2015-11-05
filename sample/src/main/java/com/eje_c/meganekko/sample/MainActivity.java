dcaa437b9bf759b0f231aa8a36cfb1af7420156d
Yuya Matsuo <matsuo@eje-c.com>
2015-11-05 19:20:26 +0900

    Update sample

--- sample/src/main/java/com/eje_c/meganekko/sample/MainActivity.java
+++ sample/src/main/java/com/eje_c/meganekko/sample/MainActivity.java
@@ -1,18 +1,31 @@
 package com.eje_c.meganekko.sample;
 
-import com.eje_c.meganekko.AndroidResource;
+import android.animation.ObjectAnimator;
+
 import com.eje_c.meganekko.MeganekkoActivity;
-import com.eje_c.meganekko.Scene;
 import com.eje_c.meganekko.SceneObject;
 import com.eje_c.meganekko.VrContext;
 
 public class MainActivity extends MeganekkoActivity {
+
+    private SceneObject obj;
+
     @Override
     protected void oneTimeInit(VrContext context) {
-        Scene scene = new Scene(context);
-        SceneObject obj = new SceneObject(context, 1, 1, context.loadTexture(new AndroidResource(context, R.mipmap.ic_launcher)));
-        obj.getTransform().setPosition(0, 0, -3);
-        scene.addChildObject(obj);
-        setScene(scene);
+        parseAndSetScene(R.xml.scene);
+        obj = findObjectById(R.id.myObject);
+    }
+
+    @Override
+    public void onTouchSingle() {
+
+        // Perform animation
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ObjectAnimator anim = ObjectAnimator.ofFloat(obj, "opacity", 1, 0, 1);
+                anim.start();
+            }
+        });
     }
 }
