e7f33be1f5631eb8315cc8b0c4df4dc84b30b161
Yuya Matsuo <matsuo@eje-c.com>
2016-01-26 13:28:36 +0900

    Add javadoc

--- library/src/main/java/com/eje_c/meganekko/Meganekko.java
+++ library/src/main/java/com/eje_c/meganekko/Meganekko.java
@@ -5,27 +5,80 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+/**
+ * The interface to Meganekko Framework system.
+ * Currently implementation is only MeganekkoActivity.
+ */
 public interface Meganekko {
 
+    /**
+     * Parse
+     *
+     * @param xmlRes
+     */
     void setSceneFromXML(int xmlRes);
 
+    /**
+     * @return Current {@link Scene}.
+     */
     Scene getScene();
 
+    /**
+     * Set {@link Scene}.
+     *
+     * @param scene
+     */
     void setScene(Scene scene);
 
+    /**
+     * Call this when background thread or UI-thread wants to notify callback on GL-thread.
+     *
+     * @param runnable Runnable wants to be run on GL-thread.
+     */
     void runOnGlThread(Runnable runnable);
 
+    /**
+     * Call this when background thread or GL-thread wants to notify callback on UI-thread.
+     *
+     * @param runnable Runnable wants to be run on UI-thread.
+     */
     void runOnUiThread(Runnable runnable);
 
+    /**
+     * Reset head tracking forward direction.
+     * Currently, it works only while device is attached to Gear VR.
+     */
     void recenter();
 
+    /**
+     * Call this to run {@code Animator} on UI-thread.
+     *
+     * @param anim        Animator
+     * @param endCallback Optional callback. This will called on GL-thread after animation.
+     */
     void animate(@NonNull final Animator anim, @Nullable final Runnable endCallback);
 
+    /**
+     * Call this to cancel {@code Animator} on UI-thread.
+     *
+     * @param anim        Animator
+     * @param endCallback Optional callback. This will called on GL-thread after cancel.
+     */
     void cancel(@NonNull final Animator anim, @Nullable final Runnable endCallback);
 
+    /**
+     * @return Current {@code Context}
+     */
     Context getContext();
 
+    /**
+     * @return {@link VrFrame}
+     */
     VrFrame getVrFrame();
 
+    /**
+     * @param object Target object.
+     * @return If user is looking at object, return true. Otherwise false.
+     */
     boolean isLookingAt(SceneObject object);
 }
--- library/src/main/java/com/eje_c/meganekko/MeganekkoApp.java
+++ library/src/main/java/com/eje_c/meganekko/MeganekkoApp.java
@@ -1,13 +1,42 @@
 package com.eje_c.meganekko;
 
+/**
+ * The interface for your application.
+ */
 public interface MeganekkoApp {
+    /**
+     * Will be called when initialization.
+     * You will call {@link Meganekko#setSceneFromXML(int)} to set rendering scene.
+     *
+     * @param meganekko Meganekko system
+     */
     void init(Meganekko meganekko);
 
+    /**
+     * Will be called on frame update.
+     * Any animations or input handlings will be implemented in it.
+     *
+     * @param meganekko Meganekko system
+     * @param vrFrame   Frame information
+     */
     void update(Meganekko meganekko, VrFrame vrFrame);
 
+    /**
+     * Will be called when user leaves from app.
+     *
+     * @param meganekko Meganekko system
+     */
     void shutdown(Meganekko meganekko);
 
+    /**
+     * Will be called when user is resumed from sleeping.
+     * Note: This method is called from UI-thread.
+     */
     void onResume();
 
+    /**
+     * Will be called when device is going to sleep mode.
+     * Note: This method is called from UI-thread.
+     */
     void onPause();
 }
