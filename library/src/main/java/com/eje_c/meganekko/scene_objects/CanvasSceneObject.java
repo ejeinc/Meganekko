6ee5c00d5895257e02664bbeab94c1a2a394c71c
Yuya Matsuo <matsuo@eje-c.com>
2016-02-01 22:26:00 +0900

    Deprecate CanvasSceneObject

--- library/src/main/java/com/eje_c/meganekko/scene_objects/CanvasSceneObject.java
+++ library/src/main/java/com/eje_c/meganekko/scene_objects/CanvasSceneObject.java
@@ -21,16 +21,17 @@ import android.graphics.Canvas;
 import android.graphics.SurfaceTexture;
 import android.view.Surface;
 
+import com.eje_c.meganekko.Frame;
 import com.eje_c.meganekko.Material;
 import com.eje_c.meganekko.RenderData;
 import com.eje_c.meganekko.SceneObject;
-import com.eje_c.meganekko.Frame;
 
 /**
  * A {@linkplain SceneObject scene object} that renders like a standard Android
  * Views. You have to call {@link #setCanvasSize(int, int)} to set Canvas size.
  * Actual drawing is done with subclass of {@link OnDrawListener}.
  */
+@Deprecated
 public class CanvasSceneObject extends SceneObject {
 
     private final SurfaceTexture mSurfaceTexture;
