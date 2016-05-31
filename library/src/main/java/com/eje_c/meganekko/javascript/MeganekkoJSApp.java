671d8bd941bfe897a0c7e9bc388437af41625b09
Yuya Matsuo <matsuo@eje-c.com>
2016-05-31 15:20:51 +0900

    Improve Meganekko.js

--- library/src/main/java/com/eje_c/meganekko/javascript/MeganekkoJSApp.java
+++ library/src/main/java/com/eje_c/meganekko/javascript/MeganekkoJSApp.java
@@ -1,9 +1,5 @@
 package com.eje_c.meganekko.javascript;
 
-import android.app.Activity;
-import android.content.pm.ActivityInfo;
-import android.content.pm.PackageManager;
-
 import com.eje_c.meganekko.Meganekko;
 import com.eje_c.meganekko.MeganekkoApp;
 
@@ -14,15 +10,9 @@ public class MeganekkoJSApp extends MeganekkoApp {
 
     public MeganekkoJSApp(Meganekko meganekko) {
         super(meganekko);
+    }
 
-        try {
-            ActivityInfo info = getContext().getPackageManager().getActivityInfo(((Activity) getContext()).getComponentName(), PackageManager.GET_META_DATA);
-            String main = info.metaData.getString("com.eje_c.meganekko.js.main");
-            if (main != null) {
-                JS.execURL(URI.create(main));
-            }
-        } catch (PackageManager.NameNotFoundException | IOException e) {
-            e.printStackTrace();
-        }
+    public void runScriptFromUri(String uri) throws IOException {
+        JS.execURL(URI.create(uri));
     }
 }
