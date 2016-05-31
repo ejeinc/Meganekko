21c5e54bf0bba9f3e54349407ca1bd5e969c316c
Yuya Matsuo <matsuo@eje-c.com>
2016-05-31 19:53:06 +0900

    Add comments

--- library/src/main/java/com/eje_c/meganekko/javascript/Http.java
+++ library/src/main/java/com/eje_c/meganekko/javascript/Http.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 eje inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.eje_c.meganekko.javascript;
 
 import android.graphics.Bitmap;
@@ -12,8 +28,10 @@ import com.android.volley.toolbox.Volley;
 import com.eje_c.meganekko.MeganekkoApp;
 
 /**
+ * JavaScript <code>http</code> module.
  * <pre>
- *     http.get('http://yourhost.com/data.json', function(err, data) {
+ *     // Get string
+ *     http.get('http://yourhost.com/data.json', function(err, str) {
  *
  *        // Error handling
  *        if (err) {
@@ -22,6 +40,23 @@ import com.eje_c.meganekko.MeganekkoApp;
  *        }
  *
  *        // do something with data
+ *        var data = JSON.parse(str);
+ *
+ *     });
+ *
+ *     // Get bitmap image
+ *     http.getImage('http://yourhost.com/data.json', function(err, image) {
+ *
+ *        // Error handling
+ *        if (err) {
+ *            err.printStackTrace();
+ *            return;
+ *        }
+ *
+ *        // do something with image
+ *        var obj = SceneObject.from(image);
+ *        obj.position(new Vector3f(0, 0, -3));
+ *        scene.add(obj);
  *
  *     });
  * </pre>
