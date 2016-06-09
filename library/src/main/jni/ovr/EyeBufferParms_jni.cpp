bbb109dc441d4af29149a82efe3026fc6fda113a
Yuya Matsuo <matsuo@eje-c.com>
2016-06-09 20:40:36 +0900

    Refactor codes

--- library/src/main/jni/ovr/EyeBufferParms_jni.cpp
+++ library/src/main/jni/ovr/EyeBufferParms_jni.cpp
@@ -26,50 +26,50 @@ extern "C"
 
 jint Java_ovr_EyeBufferParms_getResolutionWidth(JNIEnv * jni, jclass clazz, jlong nativePtr)
 {
-	ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
-	return eyeBufferParms->resolutionWidth;
+    ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
+    return eyeBufferParms->resolutionWidth;
 }
 
 void Java_ovr_EyeBufferParms_setResolutionWidth(JNIEnv * jni, jclass clazz, jlong nativePtr, jint resolutionWidth)
 {
-	ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
-	eyeBufferParms->resolutionWidth = resolutionWidth;
+    ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
+    eyeBufferParms->resolutionWidth = resolutionWidth;
 }
 
 jint Java_ovr_EyeBufferParms_getResolutionHeight(JNIEnv * jni, jclass clazz, jlong nativePtr)
 {
-	ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
-	return eyeBufferParms->resolutionHeight;
+    ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
+    return eyeBufferParms->resolutionHeight;
 }
 
 void Java_ovr_EyeBufferParms_setResolutionHeight(JNIEnv * jni, jclass clazz, jlong nativePtr, jint resolutionHeight)
 {
-	ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
-	eyeBufferParms->resolutionHeight = resolutionHeight;
+    ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
+    eyeBufferParms->resolutionHeight = resolutionHeight;
 }
 
 jint Java_ovr_EyeBufferParms_getMultisamples(JNIEnv * jni, jclass clazz, jlong nativePtr)
 {
-	ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
-	return eyeBufferParms->multisamples;
+    ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
+    return eyeBufferParms->multisamples;
 }
 
 void Java_ovr_EyeBufferParms_setMultisamples(JNIEnv * jni, jclass clazz, jlong nativePtr, jint multisamples)
 {
-	ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
-	eyeBufferParms->multisamples = multisamples;
+    ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
+    eyeBufferParms->multisamples = multisamples;
 }
 
 jboolean Java_ovr_EyeBufferParms_getResolveDepth(JNIEnv * jni, jclass clazz, jlong nativePtr)
 {
-	ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
-	return eyeBufferParms->resolveDepth;
+    ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
+    return eyeBufferParms->resolveDepth;
 }
 
 void Java_ovr_EyeBufferParms_setResolveDepth(JNIEnv * jni, jclass clazz, jlong nativePtr, jboolean resolveDepth)
 {
-	ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
-	eyeBufferParms->resolveDepth = resolveDepth;
+    ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
+    eyeBufferParms->resolveDepth = resolveDepth;
 }
 
 } // extern "C"
--- library/src/main/jni/ovr/app_jni.cpp
+++ library/src/main/jni/ovr/app_jni.cpp
@@ -26,55 +26,20 @@ extern "C"
 
 void Java_ovr_App_startSystemActivity(JNIEnv * jni, jclass clazz, jlong appPtr, jstring command)
 {
-	JavaUTFChars str(jni, command);
+    JavaUTFChars str(jni, command);
     return ((App *) appPtr)->StartSystemActivity(str.ToStr());
 }
 
-//void Java_ovr_App_returnToHome(JNIEnv * jni, jclass clazz, jlong appPtr)
-//{
-//    return ((App *) appPtr)->ReturnToHome();
-//}
-
-//jint Java_ovr_App_getSystemBrightness(JNIEnv * jni, jclass clazz, jlong appPtr)
-//{
-//    return ((App *) appPtr)->GetSystemBrightness();
-//}
-
-//void Java_ovr_App_setSystemBrightness(JNIEnv * jni, jclass clazz, jlong appPtr, jint brightness)
-//{
-//    ((App *) appPtr)->SetSystemBrightness(brightness);
-//}
-
-//jboolean Java_ovr_App_getComfortMode(JNIEnv * jni, jclass clazz, jlong appPtr)
-//{
-//    return ((App *) appPtr)->GetComfortMode();
-//}
-
-//void Java_ovr_App_setComfortMode(JNIEnv * jni, jclass clazz, jlong appPtr, jboolean enable)
-//{
-//    ((App *) appPtr)->SetComfortMode(enable);
-//}
-
-//jboolean Java_ovr_App_getDoNotDisturbMode(JNIEnv * jni, jclass clazz, jlong appPtr)
-//{
-//    return ((App *) appPtr)->GetDoNotDisturbMode();
-//}
-
-//void Java_ovr_App_setDoNotDisturbMode(JNIEnv * jni, jclass clazz, jlong appPtr, jboolean enable)
-//{
-//    ((App *) appPtr)->SetDoNotDisturbMode(enable);
-//}
-
 jlong Java_ovr_App_getEyeBufferParms(JNIEnv * jni, jclass clazz, jlong appPtr)
 {
-	const ovrEyeBufferParms & eyeBufferParms = ((App *) appPtr)->GetEyeBufferParms();
-	return (jlong)(intptr_t)&eyeBufferParms;
+    const ovrEyeBufferParms & eyeBufferParms = ((App *) appPtr)->GetEyeBufferParms();
+    return (jlong)(intptr_t)&eyeBufferParms;
 }
 
 void Java_ovr_App_setEyeBufferParms(JNIEnv * jni, jclass clazz, jlong appPtr, jlong nativePtr)
 {
-	ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
-	((App *) appPtr)->SetEyeBufferParms(*eyeBufferParms);
+    ovrEyeBufferParms * eyeBufferParms = reinterpret_cast<ovrEyeBufferParms *>(nativePtr);
+    ((App *) appPtr)->SetEyeBufferParms(*eyeBufferParms);
 }
 
 jint Java_ovr_App_getCpuLevel(JNIEnv * jni, jclass clazz, jlong appPtr)
@@ -119,11 +84,9 @@ void Java_ovr_App_setMinimumVsyncs(JNIEnv * jni, jclass clazz, jlong appPtr, jin
 
 void Java_ovr_App_showInfoText(JNIEnv * jni, jclass clazz, jlong appPtr, jfloat duration, jstring fmt)
 {
-	JavaUTFChars str(jni, fmt);
+    JavaUTFChars str(jni, fmt);
     ((App *) appPtr)->ShowInfoText(duration, str.ToStr());
 }
-
-
 
 } // extern "C"
 
