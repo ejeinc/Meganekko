0d2965c6d20c981181e73aa65e2eb5629f4a8739
Yuya Matsuo <matsuo@eje-c.com>
2016-07-05 18:38:52 +0900

    revert to NDK_TOOLCHAIN_VERSION := 4.9

--- library/src/main/jni/Application.mk
+++ library/src/main/jni/Application.mk
@@ -16,6 +16,7 @@
 
 APP_ABI := armeabi-v7a
 APP_PLATFORM := android-19
-NDK_TOOLCHAIN_VERSION := clang
+APP_STL := gnustl_static
+NDK_TOOLCHAIN_VERSION := 4.9
 
 NDK_MODULE_PATH := $(OVR_SDK_MOBILE)
