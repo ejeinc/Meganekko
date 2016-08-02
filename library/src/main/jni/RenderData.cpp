260922a5d20bef25c88d47b8b445777aac49aae0
Yuya Matsuo <matsuo@eje-c.com>
2016-08-02 12:47:05 +0900

    Separate MVP matrix

--- library/src/main/jni/RenderData.cpp
+++ library/src/main/jni/RenderData.cpp
@@ -27,12 +27,14 @@ static const char * ImageExternalDirectives =
 static const char VERTEX_SHADER[] =
         "in vec4 Position;\n"
         "in vec2 TexCoord;\n"
-        "uniform highp mat4 Mvpm;\n"
+        "uniform highp mat4 ProjM;\n"
+        "uniform highp mat4 ViewM;\n"
+        "uniform highp mat4 ModelM;\n"
         "uniform highp mat4 Texm;\n"
         "out highp vec2 oTexCoord;\n"
         "void main() {\n"
         "  oTexCoord = vec2(Texm * vec4(TexCoord, 0, 1));\n"
-        "  gl_Position = Mvpm * Position;\n"
+        "  gl_Position = ProjM * ViewM * ModelM * Position;\n"
         "}\n";
 
 static const char FRAGMENT_SHADER[] =
@@ -61,11 +63,13 @@ RenderData::RenderData() : Component(),
                             drawMode(GL_TRIANGLES) {
 
     ovrProgramParm parms[] = {
-        {"Mvpm",         ovrProgramParmType::FLOAT_MATRIX4},
         {"UniformColor", ovrProgramParmType::FLOAT_VECTOR4},
         {"Texture0",     ovrProgramParmType::TEXTURE_SAMPLED},
         {"Opacity",      ovrProgramParmType::FLOAT},
-        {"Texm",         ovrProgramParmType::FLOAT_MATRIX4}
+        {"Texm",         ovrProgramParmType::FLOAT_MATRIX4},
+        {"ProjM",        ovrProgramParmType::FLOAT_MATRIX4},
+        {"ViewM",        ovrProgramParmType::FLOAT_MATRIX4},
+        {"ModelM",       ovrProgramParmType::FLOAT_MATRIX4},
     };
     program = GlProgram::Build(NULL, VERTEX_SHADER, ImageExternalDirectives, FRAGMENT_SHADER,
                                parms, sizeof(parms) / sizeof(ovrProgramParm));
@@ -82,13 +86,14 @@ void RenderData::Render(const Matrix4f & modelM, const Matrix4f & viewM, const M
 
     GL(glUseProgram(program.Program));
 
-    Matrix4f mvpMatrix = projectionM * viewM * modelM;
-    GL(glUniformMatrix4fv(program.Uniforms[0].Location, 1, GL_TRUE, mvpMatrix.M[0]));
-    GL(glUniformMatrix4fv(program.Uniforms[4].Location, 1, GL_TRUE, TexmForVideo(material->GetStereoMode(), eye).M[ 0 ] ));
+    GL(glUniformMatrix4fv(program.Uniforms[4].Location, 1, GL_TRUE, projectionM.M[0]));
+    GL(glUniformMatrix4fv(program.Uniforms[5].Location, 1, GL_TRUE, viewM.M[0]));
+    GL(glUniformMatrix4fv(program.Uniforms[6].Location, 1, GL_TRUE, modelM.M[0]));
+    GL(glUniformMatrix4fv(program.Uniforms[3].Location, 1, GL_TRUE, TexmForVideo(material->GetStereoMode(), eye).M[ 0 ] ));
     GL(glActiveTexture (GL_TEXTURE0));
     GL(glBindTexture(GL_TEXTURE_EXTERNAL_OES, material->GetTextureId()));
-    GL(glUniform4f(program.Uniforms[1].Location, color.x, color.y, color.z, color.w));
-    GL(glUniform1f(program.Uniforms[3].Location, material->GetOpacity()));
+    GL(glUniform4f(program.Uniforms[0].Location, color.x, color.y, color.z, color.w));
+    GL(glUniform1f(program.Uniforms[2].Location, material->GetOpacity()));
 
     geometry.Draw();
 
