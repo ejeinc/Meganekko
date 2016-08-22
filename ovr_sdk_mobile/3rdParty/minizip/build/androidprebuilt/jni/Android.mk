b32dc0860aa8f8bbd8d9b86b952bdc356933519c
Yuya Matsuo <matsuo@eje-c.com>
2016-08-22 23:05:09 +0900

    add missing files

---
+++ ovr_sdk_mobile/3rdParty/minizip/build/androidprebuilt/jni/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := minizip
+
+LOCAL_SRC_FILES := ../../../lib/android/$(TARGET_ARCH_ABI)/lib$(LOCAL_MODULE).a
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
+
+LOCAL_EXPORT_C_INCLUDES :=  $(LOCAL_PATH)/../../../src
+
+ifneq (,$(wildcard $(LOCAL_PATH)/$(LOCAL_SRC_FILES)))
+  include $(PREBUILT_STATIC_LIBRARY)
+endif
