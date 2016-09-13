12db242cca2e88a9e80fad3a72023d9c78a9b0e3
Yuya Matsuo <matsuo@eje-c.com>
2016-09-13 14:19:16 +0900

    Add javadoc comments

--- library/src/main/java/org/meganekkovr/MeganekkoContext.java
+++ library/src/main/java/org/meganekkovr/MeganekkoContext.java
@@ -8,6 +8,12 @@ import org.joml.Quaternionf;
  * Abstraction interface for Activity.
  */
 interface MeganekkoContext {
+
+    /**
+     * Get android context.
+     *
+     * @return Context
+     */
     Context getContext();
 
     /**
@@ -17,11 +23,30 @@ interface MeganekkoContext {
      */
     MeganekkoApp getApp();
 
+    /**
+     * Check if user is looking at this entity.
+     *
+     * @param entity Target entity.
+     * @return {@code true} if user is looking at it. Otherwise {@code false}.
+     */
     boolean isLookingAt(Entity entity);
 
+    /**
+     * @return Head rotation.
+     */
     Quaternionf getCenterViewRotation();
 
+    /**
+     * Run some code on Android UI thread.
+     *
+     * @param command Some code to be run in UI thread.
+     */
     void runOnUiThread(Runnable command);
 
+    /**
+     * Reset forward orientation to current orientation.
+     *
+     * @param showBlack {@code true} to show black frame for a moment.
+     */
     void recenterYaw(boolean showBlack);
 }
