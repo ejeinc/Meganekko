7ec2497e5b570bfe037302929a8b03eb2f73c90a
Yuya Matsuo <niu@niusounds.com>
2017-11-15 14:16:27 +0900

    Update ExoPlayer sample library to support GVR Audio extension.

--- sample-exoplayer/src/main/java/org/meganekkovr/sampleexoplayer/App.java
+++ sample-exoplayer/src/main/java/org/meganekkovr/sampleexoplayer/App.java
@@ -1,31 +1,54 @@
 package org.meganekkovr.sampleexoplayer;
 
+import android.content.Context;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.RenderersFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.audio.AudioProcessor;
+import com.google.android.exoplayer2.ext.gvr.GvrAudioProcessor;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.extractor.ExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
+import org.joml.Quaternionf;
 import org.meganekkovr.Entity;
+import org.meganekkovr.FrameInput;
+import org.meganekkovr.HeadTransform;
 import org.meganekkovr.MeganekkoApp;
 import org.meganekkovr.SurfaceRendererComponent;
 
 public class App extends MeganekkoApp {
 
-    private SimpleExoPlayer exoPlayer;
+    private final GvrAudioProcessor gvrAudioProcessor = new GvrAudioProcessor();
+    private final SimpleExoPlayer exoPlayer;
+
+    public App(Context context) {
+
+        // Constructor is called in GearVRActivity.onCreate().
+        // It's safe to create SimpleExoPlayer because this is in main thread.
+
+        // Create custom RenderersFactory
+        RenderersFactory renderersFactory = new DefaultRenderersFactory(context) {
+            @Override
+            protected AudioProcessor[] buildAudioProcessors() {
+                return new AudioProcessor[]{gvrAudioProcessor};
+            }
+        };
+
+        exoPlayer = ExoPlayerFactory.newSimpleInstance(renderersFactory, new DefaultTrackSelector());
+    }
 
     @Override
     public void init() {
         super.init();
-
-        // Get ExoPlayer
-        // Note that ExoPlayer must be created in UI thread not in GL thread.
-        MainActivity context = (MainActivity) getContext();
-        exoPlayer = context.getExoPlayer();
 
         // Set scene
         setSceneFromXml(R.xml.scene);
@@ -49,7 +72,18 @@ public class App extends MeganekkoApp {
 
         // Connect ExoPlayer output to SurfaceRendererComponent
         exoPlayer.setVideoSurface(surfaceRenderer.getSurface());
+
+        // Start playing
         exoPlayer.setPlayWhenReady(true);
+    }
+
+    @Override
+    public void update(@NonNull FrameInput frame) {
+
+        Quaternionf quaternion = HeadTransform.getInstance().getQuaternion();
+        gvrAudioProcessor.updateOrientation(quaternion.w, quaternion.x, quaternion.y, quaternion.z);
+
+        super.update(frame);
     }
 
     @Override
